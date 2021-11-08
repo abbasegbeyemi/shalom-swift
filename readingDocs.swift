10976e48e8291af6b76871f3e9f1199b9410a61d
Abbas Egbeyemi <abbasegbeyemi@gmail.com>
2021-11-08 19:41:19 +0000

    Init commit

--- readingDocs.swift
+++ readingDocs.swift
@@ -1,16 +1,3 @@
-func splitGroup(_ names: [String], to groups: Int) {
-    // This function should split a list of names to two groups.
-    // We do this by shuffling, and the printing a formattted group list
-    let shuffledNames = names.shuffled()
-    let allNames = shuffledNames.count
-    for group in 0..<groups{
-        print("Group \(group + 1)")
-        for name in shuffledNames[(group * allNames / 2)..<((group + 1) * allNames / 2)]{
-            print("\(name)")
-        }
-    }
-}
-
 // splitGroup(["Abbas", "Ade", "Allen", "Teni", "Yemi", "Lamide", "Sarah", "Habiba", "Oyin", "Abby", "Ore", "Oskar"], to: 2)
 let numberList = [Int](1...20)
 let numberArrayList: [Int] = Array(1...20)
